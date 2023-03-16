40dc1beecc8f8484f1dde04ec943709e138ab997
liucailiang <13077991537@163.com>
2023-03-16 15:28:33 +0800

    注释@Data @AllArgsConstructor

--- src/main/java/utils/AdsClickLog.java
+++ src/main/java/utils/AdsClickLog.java
@@ -8,9 +8,9 @@ import lombok.NoArgsConstructor;
  * @author 1111358@cecdat.com
  * @version 1.0.0
  */
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+@Data   // 注在类上，提供类的get、set、equals、hashCode、toString等方法
+@AllArgsConstructor  //注在类上，提供类的全参构造
+@NoArgsConstructor  //注在类上，提供类的无参构造
 public class AdsClickLog {
     private Long userId;
     private Long adId;
